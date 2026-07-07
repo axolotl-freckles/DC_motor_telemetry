d735c4babf4c3bcd9b4ac7bfdabe8f509710ee00
acmax <aavaloscorrales@gmail.com>
2026-07-06 20:45:15 -0600

    Formatting

--- libs/encoder/encoder.cpp
+++ libs/encoder/encoder.cpp
@@ -16,9 +16,14 @@ static void IRAM_ATTR encoder_isr_handler(void *arg) {
 }
 
 Encoder::Encoder(gpio_num_t pin_a, gpio_num_t pin_b, uint16_t pulses_per_rev)
-    : _pin_a(pin_a), _pin_b(pin_b), _pulses_per_rev(pulses_per_rev),
-      _last_pulse_time_us(0), _pulse_interval_us(0), _direction(1) {
-}
+:
+    _pin_a(pin_a),
+    _pin_b(pin_b),
+    _pulses_per_rev(pulses_per_rev),
+    _last_pulse_time_us(0),
+    _pulse_interval_us (0),
+    _direction(1)
+{ }
 
 Encoder::~Encoder() {
     gpio_isr_handler_remove(_pin_a);
@@ -26,13 +31,14 @@ Encoder::~Encoder() {
 
 void Encoder::init() {
     gpio_config_t io_conf = {};
-    io_conf.intr_type = GPIO_INTR_POSEDGE;
-    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.intr_type    = GPIO_INTR_POSEDGE;
+    io_conf.mode         = GPIO_MODE_INPUT;
     io_conf.pin_bit_mask = (1ULL << _pin_a) | (1ULL << _pin_b);
     io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
-    io_conf.pull_up_en = GPIO_PULLUP_ENABLE;
+    io_conf.pull_up_en   = GPIO_PULLUP_ENABLE;
 
     ESP_ERROR_CHECK(gpio_config(&io_conf));
+
     esp_err_t install_result = gpio_install_isr_service(0);
     switch (install_result) {
         case ESP_OK:
@@ -57,16 +63,16 @@ void Encoder::init() {
 
 void IRAM_ATTR Encoder::handlePulse() {
     uint64_t now = esp_timer_get_time();
-    _pulse_interval_us = now - _last_pulse_time_us;
+    _pulse_interval_us  = now - _last_pulse_time_us;
     _last_pulse_time_us = now;
-    _direction = gpio_get_level(_pin_b) ? 1 : -1;
+    _direction          = gpio_get_level(_pin_b) ? 1 : -1;
 }
 
 float Encoder::getRpm() const {
-    uint64_t now_us = esp_timer_get_time();
-    uint64_t interval = _pulse_interval_us;
+    uint64_t now_us     = esp_timer_get_time();
+    uint64_t interval   = _pulse_interval_us;
     uint64_t last_pulse = _last_pulse_time_us;
-    int dir = _direction;
+    int      dir        = _direction;
 
     if ((now_us - last_pulse) > IDLE_RESET_TIME_ms) {
         return 0.0f;
@@ -81,8 +87,8 @@ float Encoder::getRpm() const {
 
 void Encoder::reset() {
     _last_pulse_time_us = esp_timer_get_time();
-    _pulse_interval_us = 0;
-    _direction = 1;
+    _pulse_interval_us  = 0;
+    _direction          = 1;
 }
 
 int32_t Encoder::getDirection() const {
--- libs/encoder/encoder.hpp
+++ libs/encoder/encoder.hpp
@@ -5,22 +5,27 @@
 
 class Encoder {
 public:
-    Encoder(gpio_num_t pin_a, gpio_num_t pin_b, uint16_t pulses_per_rev = 1600);
+    Encoder(
+        gpio_num_t pin_a,
+        gpio_num_t pin_b,
+        uint16_t pulses_per_rev = 1600
+    );
     ~Encoder();
 
     void init();
-    float getRpm() const;
     void reset();
-    int32_t getDirection() const;
-    uint64_t getLastPulseTime() const;
-    void handlePulse(); // Llamado desde ISR
 
+    float    getRpm() const;
+    int32_t  getDirection() const;
+    uint64_t getLastPulseTime() const;
+
+    void handlePulse(); // Called from ISR
 private:
     gpio_num_t _pin_a;
     gpio_num_t _pin_b;
-    uint16_t _pulses_per_rev;
+    uint16_t   _pulses_per_rev;
 
     volatile uint64_t _last_pulse_time_us;
     volatile uint64_t _pulse_interval_us;
-    volatile int _direction;
+    volatile int      _direction;
 };
